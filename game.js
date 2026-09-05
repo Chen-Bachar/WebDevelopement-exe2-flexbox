0e4fb62e5814a4ddae5dac665d211f6272602dcf
Chen-Bachar <chen45454@cs.colman.ac.il>
2026-09-05 15:02:36 +0300

    almost done, before checking

--- game.js
+++ game.js
@@ -9,9 +9,20 @@ const userCode = document.getElementById("user-code");
 const nextButton = document.getElementById("next");
 const message = document.getElementById("message");
 const countTrials = document.getElementById("count-trials");
+const resetButton = document.getElementById("reset");
+
+
 const successMessage = "כל הכבוד! עברת את השלב!";
 const errorMessage = "לא עברת את השלב, נסה שוב";
 const trials = [0, 0, 0, 0, 0, 0, 0];
+const levelButtons = [document.getElementById("level1"),
+                      document.getElementById("level2"), 
+                      document.getElementById("level3"),
+                      document.getElementById("level4"),
+                      document.getElementById("level5"),
+                      document.getElementById("level6"),
+                      document.getElementById("level7")
+                    ]
 
 
 updateIcons(3);
@@ -64,7 +75,8 @@ function getLevelInstructions(level)
 
 }
 
-function parseCSS(code) {
+function parseCSS(code) 
+{
     const styles = {};
     const rules = code.split(";");
     rules.forEach(rule => {
@@ -81,11 +93,15 @@ function parseCSS(code) {
 function resetBoard()
 {
     board.style.cssText = "display: flex;";
+    if (Number(currentLevel.textContent) == 5 || Number(currentLevel.textContent) == 6)
+    {
+        board.style.cssText += "justify-content: center";
+    }
     userCode.value = "";   
 }
 
-function updateIcons(numberOfIcons) {
-
+function updateIcons(numberOfIcons) 
+{
     board.innerHTML = "";
     for (let i = 0; i < numberOfIcons; i++) {
         const icon = document.createElement("i");
@@ -123,7 +139,7 @@ function checkLevelSolution(level)
         }
         case(5):
         {
-            success = (styles["flex-direction"] === "row" && styles["justify-content"] === "center" && styles["align-"] === "center" && styles["flex-wrap"] === "wrap");
+            success = (styles["flex-direction"] === "row" && styles["justify-content"] === "center" && styles["align-content"] === "center" && styles["flex-wrap"] === "wrap");
             break;
         }
         case(6):
@@ -149,6 +165,7 @@ function checkLevelSolution(level)
             message.textContent = "";
         }, 2000);
         setTimeout(getNextLevel, 2000);
+        levelButtons[level-1].style.cssText = "background-color: greenyellow";
     }
     else
     {
@@ -159,45 +176,74 @@ function checkLevelSolution(level)
         setTimeout(resetBoard, 2000);
     }
     trials[level-1]++;
-    countTrials.textContent=trials[level-1];
+    countTrials.textContent = trials[level-1];
+}
+
+function getNumOfIcons()
+{
+    updateIcons(1); 
+    const icon = document.querySelector("#game-board i");
+    return Math.floor(board.clientWidth/icon.getBoundingClientRect().width) + 2;
+}
+
+function getLevel(level)
+{
+    resetBoard();
+    currentLevel.textContent = level;
+    getLevelInstructions(level);
+    if (level == 5 || level == 6)
+    {
+        updateIcons(getNumOfIcons());
+        board.style.cssText += "justify-content: center";
+    }
+    else
+        updateIcons(3);
 }
 
 function getNextLevel()
 {
-    resetBoard();
     let currentLevelNum = Number(currentLevel.textContent);
     if(currentLevelNum < Number(totalLevels.textContent))
     {
-        currentLevel.textContent = currentLevelNum + 1;
         currentLevelNum++;
+        getLevel(currentLevelNum);
     }
-    getLevelInstructions(currentLevelNum);
-    if (currentLevelNum == 5 || currentLevelNum == 6)
-        updateIcons(13);
-    else
-        updateIcons(3);
 }
 
 function getPrevLevel()
 {
-    resetBoard();
     let currentLevelNum = Number(currentLevel.textContent);
     if(currentLevelNum > 1)
     {
-        currentLevel.textContent = currentLevelNum - 1;
         currentLevelNum--;
+        getLevel(currentLevelNum);
     }
-    getLevelInstructions(currentLevelNum);
-    if (currentLevelNum == 5 || currentLevelNum == 6)
-        updateIcons(13);
-    else
-        updateIcons(3);
+}
+
+function nextButtonClick()
+{
+    adaptUserCode();
+    checkLevelSolution(currentLevel.textContent);
+}
+
+function adaptUserCode()
+{
+    const styles = parseCSS(userCode.value);
+    for (const property in styles) {
+        board.style.setProperty(property, styles[property]);
+    }
 }
 
 //EVENT LISTENERS
 nextArrow.addEventListener("click", getNextLevel);
 prevArrow.addEventListener("click",getPrevLevel);
-nextButton.addEventListener("click", function() {
-    board.style.cssText = userCode.value;
-    checkLevelSolution(currentLevel.textContent);
-});
+nextButton.addEventListener("click", nextButtonClick);
+resetButton.addEventListener("click", resetBoard);
+userCode.addEventListener("input", adaptUserCode);
+levelButtons[0].addEventListener("click", function(){getLevel(1)});
+levelButtons[1].addEventListener("click", function(){getLevel(2)});
+levelButtons[2].addEventListener("click", function(){getLevel(3)});
+levelButtons[3].addEventListener("click", function(){getLevel(4)});
+levelButtons[4].addEventListener("click", function(){getLevel(5)});
+levelButtons[5].addEventListener("click", function(){getLevel(6)});
+levelButtons[6].addEventListener("click", function(){getLevel(7)});
